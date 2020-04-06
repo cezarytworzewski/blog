9890273209b404d9eb4b66373cebebf2de58afcb
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-06 18:02:52 +0200

    j

---
+++ czarek.sh
@@ -0,0 +1,126 @@
+Czarek@CZAREK-DOM MINGW64 /e/Projekty/blog (master)
+$ hexo deploy
+
+Czarek@CZAREK-DOM MINGW64 /e/Projekty/blog (master)
+$ hexo clean
+INFO  Deleted database.
+INFO  Deleted public folder.
+
+Czarek@CZAREK-DOM MINGW64 /e/Projekty/blog (master)
+$ hexo deploy
+INFO  Start processing
+INFO  Files loaded in 758 ms
+WARN  No layout: kontakt/index.html
+WARN  No layout: 2020/04/06/instalacja-konfiguracja-serwera-SSH-Debian/index.html
+WARN  No layout: 2020/02/16/test rembember/index.html
+WARN  No layout: 2018/09/09/moja-pierwsza-praca-w-IT/index.html
+WARN  No layout: 2018/07/01/latwy-pasek-postepu-w-bootstrap/index.html
+WARN  No layout: 2018/06/21/dostosowanie-umiejetnosci-pod-rekrutacje/index.html
+WARN  No layout: 2018/06/20/bootstrap-nauka/index.html
+WARN  No layout: 2018/06/16/jak-dodac-artykul-na-bloga-hexo/index.html
+WARN  No layout: 2018/06/15/dlaczego-nie-mozna-modyfikowac-pliku-bootstrapmincss/index.html
+WARN  No layout: 2018/06/15/porownanie-znacznikow-q-cite-blockquote/index.html
+WARN  No layout: 2018/06/15/regula-wlasciwosc/index.html
+WARN  No layout: 2018/06/14/img-vs-background-image/index.html
+WARN  No layout: 2018/06/13/podstawy-js-2/index.html
+WARN  No layout: 2018/06/08/podstawy-linux-2/index.html
+WARN  No layout: 2018/06/07/podstawy-linux-1/index.html
+WARN  No layout: 2018/06/05/podstawy-js-1/index.html
+WARN  No layout: 2018/06/03/javascript-od-nowa/index.html
+WARN  No layout: 2018/06/02/co-dalej-z-moim-marzeniem/index.html
+WARN  No layout: 2018/06/01/linux-vs-windows/index.html
+WARN  No layout: 2018/05/31/git-ssh/index.html
+WARN  No layout: 2018/05/30/omnie/index.html
+WARN  No layout: archives/index.html
+WARN  No layout: archives/page/2/index.html
+WARN  No layout: archives/2018/index.html
+WARN  No layout: archives/2018/page/2/index.html
+WARN  No layout: archives/2018/05/index.html
+WARN  No layout: archives/2018/06/index.html
+WARN  No layout: archives/2018/06/page/2/index.html
+WARN  No layout: archives/2018/07/index.html
+WARN  No layout: archives/2018/09/index.html
+WARN  No layout: archives/2020/index.html
+WARN  No layout: archives/2020/02/index.html
+WARN  No layout: archives/2020/04/index.html
+WARN  No layout: index.html
+WARN  No layout: page/2/index.html
+INFO  Generated: atom.xml
+INFO  Generated: sitemap.xml
+INFO  Generated: index.html
+INFO  Generated: kontakt/index.html
+INFO  Generated: archives/index.html
+INFO  Generated: archives/page/2/index.html
+INFO  Generated: archives/2018/05/index.html
+INFO  Generated: archives/2020/02/index.html
+INFO  Generated: page/2/index.html
+INFO  Generated: archives/2018/07/index.html
+INFO  Generated: archives/2018/09/index.html
+INFO  Generated: images/pasek-postępu-2.png
+INFO  Generated: archives/2020/04/index.html
+INFO  Generated: images/pasek-postępu.png
+INFO  Generated: images/pasek-postępu-wzorek.png
+INFO  Generated: archives/2018/index.html
+INFO  Generated: archives/2018/06/index.html
+INFO  Generated: archives/2020/index.html
+INFO  Generated: images/front.jpg
+INFO  Generated: images/workflow.jpg
+INFO  Generated: archives/2018/06/page/2/index.html
+INFO  Generated: archives/2018/page/2/index.html
+INFO  Generated: 2020/04/06/instalacja-konfiguracja-serwera-SSH-Debian/index.html
+INFO  Generated: 2018/09/09/moja-pierwsza-praca-w-IT/index.html
+INFO  Generated: 2020/02/16/test rembember/index.html
+INFO  Generated: 2018/07/01/latwy-pasek-postepu-w-bootstrap/index.html
+INFO  Generated: 2018/06/21/dostosowanie-umiejetnosci-pod-rekrutacje/index.html
+INFO  Generated: 2018/06/20/bootstrap-nauka/index.html
+INFO  Generated: 2018/06/16/jak-dodac-artykul-na-bloga-hexo/index.html
+INFO  Generated: 2018/06/15/dlaczego-nie-mozna-modyfikowac-pliku-bootstrapmincss/index.html
+INFO  Generated: 2018/06/14/img-vs-background-image/index.html
+INFO  Generated: 2018/06/13/podstawy-js-2/index.html
+INFO  Generated: 2018/06/08/podstawy-linux-2/index.html
+INFO  Generated: 2018/06/15/regula-wlasciwosc/index.html
+INFO  Generated: 2018/06/15/porownanie-znacznikow-q-cite-blockquote/index.html
+INFO  Generated: 2018/06/07/podstawy-linux-1/index.html
+INFO  Generated: 2018/06/05/podstawy-js-1/index.html
+INFO  Generated: 2018/06/03/javascript-od-nowa/index.html
+INFO  Generated: 2018/06/02/co-dalej-z-moim-marzeniem/index.html
+INFO  Generated: 2018/06/01/linux-vs-windows/index.html
+INFO  Generated: 2018/05/31/git-ssh/index.html
+INFO  Generated: 2018/05/30/omnie/index.html
+INFO  42 files generated in 100 ms
+
+Czarek@CZAREK-DOM MINGW64 /e/Projekty/blog (master)
+$ npm run deploy
+
+> blog@0.0.0 predeploy E:\Projekty\blog
+> npm run lint:all
+
+
+> blog@0.0.0 lint:all E:\Projekty\blog
+> markdownlint source/
+
+'markdownlint' is not recognized as an internal or external command,
+operable program or batch file.
+npm ERR! code ELIFECYCLE
+npm ERR! errno 1
+npm ERR! blog@0.0.0 lint:all: `markdownlint source/`
+npm ERR! Exit status 1
+npm ERR!
+npm ERR! Failed at the blog@0.0.0 lint:all script.
+npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\Czarek\AppData\Roaming\npm-cache\_logs\2020-04-06T15_50_51_703Z-debug.log
+npm ERR! code ELIFECYCLE
+npm ERR! errno 1
+npm ERR! blog@0.0.0 predeploy: `npm run lint:all`
+npm ERR! Exit status 1
+npm ERR!
+npm ERR! Failed at the blog@0.0.0 predeploy script.
+npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\Czarek\AppData\Roaming\npm-cache\_logs\2020-04-06T15_50_51_733Z-debug.log
+
+Czarek@CZAREK-DOM MINGW64 /e/Projekty/blog (master)
+$
